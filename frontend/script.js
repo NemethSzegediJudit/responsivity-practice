3ee9cd5c7b71805eb3cb631cecadebea7d9a5ad1
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-05-02 10:18:21 +0200

    work on script.js on the lesson

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,41 @@
-const loadEvent = function () {
-    const rootElement = document.getElementById("root");
+const headerComponent = function (title) {
+    return `
+    <header>${title}</header>
+    `
 }
 
-window.addEventListener("load", loadEvent)
+const sectionComponent = function (id, buttontext, h2text) {
+    return `
+    <section id="${id}"> 
+        <h2>${h2text}</h2>
+        <button>${buttontext}</button>
+    </section>
+    `
+}
+//idézőjel végére figyelni, backtick helyére figyelni, zárótagek helyére figyelni
+
+const footerComponent = function () {
+    return `
+    <footer></footer>
+    `
+}
+
+const loadEvent = function () {
+    const rootElement = document.getElementById("root")
+
+    rootElement.insertAdjacentHTML("beforebegin", headerComponent("Responsivity practice")); //az insert ajacenthtml megvárja, amíg lefut a headercomponent
+
+    let sections = ""
+
+    for (let index = 1; index < 5; index++) {
+        sections += sectionComponent(`id-${index}`, `Button ${index}`, `Subtitle ${index}`) //id-ba nem tehetünk szóközt, ezért van ott kötőjel
+    }
+
+    rootElement.insertAdjacentHTML("beforeend", sections);
+
+    rootElement.insertAdjacentHTML("afterend", footerComponent());
+}
+
+window.addEventListener("load", loadEvent) //csak akkor indítsa a loadeventet, amikor a load(az oldal betöltése) már megtörtént
+
+//for: ha nincs adatunk. for of: ha van adatunk. 
